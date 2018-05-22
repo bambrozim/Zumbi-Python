4527c5e980f7bbd3b976afb354725882295eb67a
Bruna Ambrozim Silveira <b.ambrozim@gmail.com>
2018-05-22 09:38:16 -0300

    Melhoria de código

--- 003_Telefonia_if else.py
+++ 003_Telefonia_if else.py
@@ -3,15 +3,13 @@ minutos = int(input("Minutos utilizados: "))
 if minutos < 0:
     print("Digite uma quantia válida!")
 else:
-    if minutos >= 0 and minutos < 200:
+    if minutos < 200:
         conta = .2*minutos
+    elif minutos <= 400:
+        conta = .18*minutos
+    elif minutos <= 800:
+        conta = .15*minutos
     else:
-        if minutos >= 200 and minutos <= 400:
-            conta = .18*minutos
-        else:
-            if minutos > 400 and minutos <= 800:
-                conta = .15*minutos
-            else:
-                conta = .8*minutos
+       conta = .8*minutos
 
     print("Sua conta é de R$ %.2f reais." %conta)
